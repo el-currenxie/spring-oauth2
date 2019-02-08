84fc84c1d270e0f0057c3775da03c745628564b5
edward <edward.lam@currenxie.com>
2019-02-08 01:24:24 +0000

    done node-service

--- node-service/index.js
+++ node-service/index.js
@@ -1,64 +1,62 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const axios = require('axios')
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
 
-const app = express()
-const port = process.env.PORT || 3000
+const app = express();
+const port = process.env.PORT || 3000;
 
 
-app.use(bodyParser.json())
-
+app.use(bodyParser.json());
 
 const credentials = {
-    client: {
-        id: '123',
-        secret: '123'
-    },
-    auth: {
-        tokenHost: 'http://localhost:8080'
-    }
+  client: {
+    id: '123',
+    secret: '123'
+  },
+  auth: {
+    tokenHost: 'http://localhost:8080'
+  }
 };
 const oauth2 = require('simple-oauth2').create(credentials);
-let assessToken
-async function prepareRoute(assessToken) {
-    try {
-        const result = await oauth2.clientCredentials.getToken();
-        console.log(result);
-        accessToken = oauth2.accessToken.create(result);
-        return accessToken
-    } catch (error) {
-        console.log('Access Token error', error.message);
-    }
+let accessToken;
+async function prepareToken() {
+  try {
+    const result = await oauth2.clientCredentials.getToken();
+    return await oauth2.accessToken.create(result);
+  } catch(error) {
+    console.log('Access Token error', error.message);
+  }
 }
 
 async function createApp(){
-  await prepareRoute(assessToken)
 
-    app.get('/', (req, res) => res.json({'yay': 'Hello World!'}))
-    app.get('/info', (req, res) => {
-        console.log(assessToken)
-        if (assessToken) {
-            var url = "http://localhost:8080/api/accounts/account";
+  accessToken = await prepareToken();
 
-            var options = {
-                method: "GET",
-                headers: {
-                    "content-type": "application/json+fhir",
-                    "authorization": "Bearer " + assessToken.access_token
-                },
-                uri: url
-            };
+  app.get('/', (req, res) => res.json({'yay': 'Hello World!'}));
+  app.get('/info', (req, res) => {
+    if (accessToken) {
+      let url = "http://localhost:8080/api/accounts/account";
 
-            request(options, function (error, response, body) {
-                res.json(body);
-            });
-        } else {
-            res.json({err:"Not logged in"});
-        }
-    })
+      let options = {
+        method: "GET",
+        headers: {
+          "content-type": "application/json",
+          "authorization": "Bearer " + accessToken.token.access_token
+        },
+        uri: url
+      };
 
+      request(options,  (error, response, body) => {
+        res.json(JSON.parse(body));
+      });
+    } else {
+      res.json({err:"Not logged in"});
+    }
+  });
 
-    app.listen(port, () => console.log(`Listening on port ${port}`))
+  app.listen(port, () => console.log(`Listening on port ${port}`))
 }
 
-createApp()
+createApp().catch(error => {
+  console.log("error", error)
+});
